88545a46af1b12901a82a7f811e2593a984f5699
mohammed-git-2006 <mdrawashdeh2006@gmail.com>
2021-07-03 09:26:12 +0300

    Change Main.cpp render mode to Orient Object

--- main.cpp
+++ main.cpp
@@ -16,242 +16,192 @@
 #include "lib/Color.hpp"
 #include "lib/Light.hpp"
 #include "lib/Scene.hpp"
+#include "lib/Resources.hpp"
 
 #include <unistd.h>
 
 Display display;
-Projection projection;
-MatrixObject view;
 
-OBJLoader stall_loader, sphere_loader;
+class Game {
+    public:
+    Projection projection;
+    MatrixObject view;
 
-Texture stall_texture;
-TextureSurface opengl_logo;
+    OBJLoader stall_loader;
 
-Light light;
+    Texture stall_texture;
+    TextureSurface opengl_logo;
 
-float camera_speed = 10;
-float mouse_sensitivity = 1.5, player_speed = 10;
+    Light light;
 
-RenderObject stall, sphere;
+    float camera_speed = 20;
+    float mouse_sensitivity = 1.5, player_speed = 10, rotation_speed = 100;
 
-void MoveCamera() {
-    if (Keyboard::isPressed(GLFW_KEY_W))
-        view.Translate(0, 0, 1 * display.delta_time * camera_speed);
-    
-    if (Keyboard::isPressed(GLFW_KEY_S))
-        view.Translate(0, 0, -1 * display.delta_time * camera_speed);
-
-    if (Keyboard::isPressed(GLFW_KEY_A))
-        view.Translate(-1 * display.delta_time * camera_speed, 0, 0);
-
-    if (Keyboard::isPressed(GLFW_KEY_D))
-        view.Translate(1 * display.delta_time * camera_speed, 0, 0);
-
-    if (Keyboard::isPressed(GLFW_KEY_PAGE_UP)) 
-        view.Translate(0, -1 * display.delta_time * camera_speed, 0);
-
-    if (Keyboard::isPressed(GLFW_KEY_PAGE_DOWN)) 
-        view.Translate(0, 1 * display.delta_time * camera_speed, 0);
-}
-
-void Load_OBJs() {
-    Debugger::Log("Load_OBJs", "Loading Stall OBJ File ...");
-    std::cout << (stall_loader.loadFromFile("res/stall.obj") == true ? "Loaded" : "Failed")  << "\n";
-    Debugger::Log("Load_OBJs", "Stall OBJ Loaded, Cleaning Temp ...");
-    stall_loader.CleanTemp();
-
-    Debugger::Log("Load_OBJs", "Loading Sphere OBJ File ...");
-    sphere_loader.loadFromFile("res/sphere.obj");
-    Debugger::Log("Load_OBJs", "Sphere OBJ Loaded, Cleaning Temp ...");
-    sphere_loader.CleanTemp();
-}
-
-float dragon_brightness = 1;
-
-void ScrollCallback(GLFWwindow* window, double x, double y) {
-    dragon_brightness += 0.2 * y;
-
-    Debugger::Log("ScrollCallback", "Dragon Brightness : " + std::to_string(dragon_brightness));
-} 
-
-glm::vec3 lookAt_Eye() {
-    return glm::vec3(
-        //sphere.model.x,
-        //sphere.model.y + 2,
-        sphere.model.x, sphere.model.y + 4,
-        sphere.model.z + 10
-    );
-
-    //return glm::vec3(0, 0, 0);
-}
-
-int main(int c, char ** args) {
-    Debugger::Log("Init", "Load_OBJs()");
-    Load_OBJs();
-
-    Debugger::Log("Init", "Initializing Display ...");
-    display.Make("GLFW window", 700, 700);
-    display.EnableBlend();
-    display.EnableDepth();
-    //display.EnableCullFace(GL_BACK);
-
-    GLFWimage* image = new GLFWimage;
-    int comp;
-    image->pixels = stbi_load("res/opengl.jpg", &image->width, &image->height, &comp, STBI_rgb_alpha);
-
-    glfwSetWindowIcon(display.glfwWindow, 1, image);
-
-    Debugger::Log("Init", "Initializing Keyboard and Mouse ...");
-    Keyboard::setDisplay(&display);
-    Mouse::setDisplay(&display);
-
-    Debugger::Log("Init", "Initializing Projection ...");
-    projection.setProjection_Perspictive(45, 1, 0.1, 200);
-
-    float sky_diff = 0.05;
-
-    display.setBackgroundColor(0, 0, 0, 0);
-    view.Translate(0, -5, 0);
-
-    stall.va.Init(0, 0);
-    stall.va.AddLayout(3, stall_loader.vertices_pointer(),  stall_loader.vertices_size * FLOAT_SIZE, GL_DYNAMIC_DRAW);
-    stall.va.AddLayout(2, stall_loader.uvs_pointer(), stall_loader.uvs_size * FLOAT_SIZE, GL_DYNAMIC_DRAW);
-    stall.va.AddLayout(3, stall_loader.normals_pointer(),   stall_loader.normals_size * FLOAT_SIZE, GL_DYNAMIC_DRAW);
-    stall.texture = CreateTexture("res/stallTexture.png");
-    stall.model.Translate(0, 0, -10);
-    stall.model.matrix = glm::rotate(stall.model.matrix, glm::radians(180.0f), glm::vec3(0, 1, 0));
-    stall_texture.loadFromFile("res/stallTexture.png");
-
-    sphere.va.Init(0, 0);
-    sphere.va.AddLayout(3, sphere_loader.vertices_pointer(), sphere_loader.vertices_size * FLOAT_SIZE, GL_DYNAMIC_DRAW);
-    sphere.va.AddLayout(2, sphere_loader.uvs_pointer(), sphere_loader.uvs_size * FLOAT_SIZE, GL_DYNAMIC_DRAW);
-    sphere.va.AddLayout(3, sphere_loader.normals_pointer(), sphere_loader.normals_size * FLOAT_SIZE, GL_DYNAMIC_DRAW);
-    sphere.model.Translate(0, 0, 0);
+    RenderObject stall;
 
     ShaderProgram shader;
-    shader.loadFromFile("res/cube_shader.hlsl");
-    shader.Compile();
 
-    std::cout << opengl_logo.texture.error << std::endl;
+    std::string LOAD_MODELS_ERROR  = "No Errors",
+                MAKE_DISPLAY_ERROR = "No Errors",
+                LOAD_SHADER_ERROR  = "No Errors";
 
-    Projection ui_projection;
-    ui_projection.setProjection_Ortho(-5, 5, -5, 5);
+    bool loadModels() {
+        Debugger::Log("loadModels", "Loading Stall Model ...");
 
+        if (! stall_loader.loadFromFile(Res::get("dragon.obj"))) {
+            LOAD_MODELS_ERROR = "Failed to load stall model";
+            return false;
+        }
 
-    Debugger::Log("Main", "Main Loop Entry");
+        Debugger::Log("loadModels", "Stall Model Loaded");
 
-    glfwSetScrollCallback(display.glfwWindow, ScrollCallback);
+        return true;
+    }
 
-    ActionManager changeMouse_M, changeDrawMode_M;
-    int mouse_disabled = false;
-    GLenum draw_mode = GL_TRIANGLES;
+    bool MakeDisplay() {
+        if (! display.Make("GLFW window", 512, 512)) {
+            MAKE_DISPLAY_ERROR = display.error;
+            return false;
+        }
 
+        display.EnableBlend();
+        display.EnableCullFace(GL_BACK);
+        display.EnableDepth();
 
-    light.lightColor = Colors::WHITE;
-    light.lightDistance = 2;
-    light.brightness = 1.2;
+        display.setBackgroundColor(144, 144, 144, 255);
 
-    Debugger::Log("ProcessInfo", "Process ID : " + std::to_string(getpid()));
+        return true;
+    }
 
-    while (! display.shouldClose() && !Keyboard::isPressed(GLFW_KEY_Q)) {
+    bool Setup() {
+        Debugger::Log("Game::Startup", "Loading OBJ Models before Initializing Display ...");
+        if (! loadModels()) {
+            Debugger::Error("Game::Startup", "Failed to load Models, models Loader error:\n\t" +
+                this->LOAD_MODELS_ERROR);
+            
+            return false;
+        }
+
+        Debugger::Log("Game::Setup", "All OBJ Models Loaded");
+        Debugger::Log("Game::Setup", "Making 512x512 Display ...");
+
+        if (! MakeDisplay()) {
+            Debugger::Error("Game::Setup", "Failed to Make Display, display manager error:\n\t"+
+                MAKE_DISPLAY_ERROR);
+            
+            return false;
+        }
+
+        Keyboard::setDisplay(&display);
+        Mouse::setDisplay(&display);
+
+        Debugger::Log("Game::Setup", "Making Stall Render Object ...");
+
+        stall.va.Init();
+        stall.va.AddLayout(
+            3, 
+            stall_loader.vertices_pointer(), 
+            stall_loader.vertices_size * FLOAT_SIZE,
+            GL_DYNAMIC_DRAW
+        );
+
+        stall.va.AddLayout(
+            2, 
+            stall_loader.uvs_pointer(), 
+            stall_loader.uvs_size * FLOAT_SIZE,
+            GL_DYNAMIC_DRAW
+        );
+
+        stall.va.AddLayout(
+            3, 
+            stall_loader.normals_pointer(), 
+            stall_loader.normals_size * FLOAT_SIZE,
+            GL_DYNAMIC_DRAW
+        );
+
+        Debugger::Log("Game::Setup", "Making Static Shader ...");
+
+        if (! shader.loadFromFile(Res::get("static_shader.hlsl"))) {
+            Debugger::Error("Game::Setup", "Failed to load Shader");
+            return false;
+        }
+
+        if (! shader.Compile()) {
+            Debugger::Error("Game::Setup", "Failed to Compile Shader Program, more info:\n"
+                + shader.error);
+            return false;
+        }
+
+        stall.model.Translate(0, 0, 0);
+
+        view.Translate(0, 0, -10);
+
+        projection.setProjection_Perspictive(60, 1, 0.1, 100);
+        light.lightColor = Colors::WHITE;
+        light.brightness = 1.5;
+        stall.texture = CreateTexture(Res::get("stallTexture.png"));
+
+        return true;
+    }
+
+    void Clear() {
         display.Clear_Depth();
-        display.Clear();
+
         display.calculateDeltaTime();
-
-        changeMouse_M.Increase(display.delta_time);
-        changeDrawMode_M.Increase(display.delta_time);
-
         Mouse::Update();
+    }
 
-        if (Keyboard::isPressed(GLFW_KEY_R)) {
-            sphere.model.Reset();
-        }
-
-        //MoveCamera();
-
-        view.Translate(
-            Mouse::delta_x * mouse_sensitivity * display.delta_time,
-            Mouse::delta_y * mouse_sensitivity * display.delta_time,
-            0
-        );
-
-        sphere.model.Translate(
-            Keyboard::horizontal() * player_speed * display.delta_time,
-            (Keyboard::isPressed(GLFW_KEY_PAGE_UP) ? 1 : (Keyboard::isPressed(GLFW_KEY_PAGE_DOWN) ? -1 : 0)) * 
-                player_speed * display.delta_time,
-            Keyboard::vertical() * -player_speed * display.delta_time
-        );
-
-        float radius = 15;
-
-        view.matrix = glm::lookAt(
-            //lookAt_Eye() ,
-            //sphere.model.GLM_Vector(),
-            //glm::vec3(0, 2, 0)
-            glm::vec3(std::sin(view.x * 0.1) * radius + sphere.model.x, 0, std::cos(view.z) * radius + sphere.model.z),
-            sphere.model.GLM_Vector(),
-            glm::vec3(0, 2, 0)
-        );
-
-        printf("view.matrix = [");
-
-        std::cout << view.matrix.length() << std::endl;
-
-        for(int i=0;i<view.matrix.length();++i) {
-            printf("%f", view.matrix[i]);
-
-            if (i == (view.matrix.length() - 1)) printf("]\n");
-            else printf(", ");
-        }
-
-        if (Keyboard::isPressed(GLFW_KEY_TAB) && changeMouse_M.isAvailable(1)) {
-            changeMouse_M.Zero();
-
-            glfwSetInputMode(display.glfwWindow, GLFW_CURSOR, (mouse_disabled ? GLFW_CURSOR_NORMAL : GLFW_CURSOR_DISABLED));
-
-            mouse_disabled = !mouse_disabled;
-        }
-
-        if (Keyboard::isPressed(GLFW_KEY_V) && changeDrawMode_M.isAvailable(0.5)) {
-            changeDrawMode_M.Zero();
-
-            draw_mode = (draw_mode == GL_TRIANGLES ? GL_QUADS : GL_TRIANGLES);
-        }
-
-        //stall.model.Rotate(1, 0, 1, 0);
-
-        shader.Use();
-        shader.UniformMatrix("projection", projection.Pointer());
-        shader.UniformMatrix("view", view.Pointer());
-        //shader.UniformVec4f("light_pos", -view.x, -view.y, -view.z, 1);
-        shader.UniformVec4f("light_pos", light.position.GLM_Vector(), 1);
-        shader.UniformVec4f("light_color", light.lightColor);
-        shader.UniformFloat("brightness", light.brightness);
-
-        shader.Disable("enable_blend");
-
-        /* Draw Stall */
-
-        shader.UniformMatrix("model", stall.model.Pointer());
-        shader.Enable("enable_texture");
-        stall.va.Bind();
-        stall.texture.Bind();
-        
-        glDrawArrays(draw_mode, 0, stall_loader.vertices_size / 3);
-
-        /* Draw Sphere */
-        sphere.va.Bind();
-        shader.Disable("enable_texture");
-        shader.UniformMatrix("model", sphere.model.Pointer());
-
-        glDrawArrays(draw_mode, 0, sphere_loader.vertices_size / 3);
-
-        display.setTitle((char*)std::to_string(1 / display.delta_time).c_str());
+    void RenderFrame() {
         display.SwapBuffers();
     }
 
-    Debugger::Log("Main", "User Closed Window");
+    void Render() {
+        while(! display.shouldClose() && ! Keyboard::isPressed(GLFW_KEY_Q)) {
+            Clear();
 
-    return 0;
+            stall.model.Rotate(rotation_speed * display.delta_time, 0, 1, 0);
+            
+            view.Translate(
+                -Keyboard::horizontal() * player_speed * display.delta_time,
+                -(Keyboard::isPressed(GLFW_KEY_PAGE_UP) ? 1 : ( (Keyboard::isPressed(GLFW_KEY_PAGE_DOWN) ? -1 : 0) )) * player_speed * display.delta_time,
+                Keyboard::vertical() * player_speed * display.delta_time
+            );
+            
+            /* Render all */
+            shader.Use();
+            shader.UniformMatrix("view",  view.Pointer());
+            shader.UniformMatrix("model", stall.model.Pointer());
+            shader.UniformMatrix("projection", projection.Pointer());
+            shader.UniformFloat("brightness", light.brightness);
+            shader.UniformVec4f("light_pos", -view.x, -view.y, -view.z, 1);
+            shader.UniformVec4f("light_color", light.lightColor);
+            shader.Disable("enable_blend");
+            shader.Disable("enable_texture");
+
+            stall.texture.Bind();
+            stall.va.Bind();
+
+            glDrawArrays(GL_TRIANGLES, 0, stall_loader.faces);
+
+            /* render frame buffer */
+
+            RenderFrame();
+        }
+    }
+};
+
+
+int main(int c, char ** args) {
+    Game game;
+    
+    if (! game.Setup()) exit(-1);
+
+    Debugger::Log("main", "Starting Main Loop ...");
+
+    game.Render();
+
+    Debugger::Log("main", "Game Main Loop Exit");
+
+    exit(1);
 }
+
