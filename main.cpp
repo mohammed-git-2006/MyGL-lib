83afcc1e73a2c51c8088305c22423351fbf8ffe5
mohammed-git-2006 <mdrawashdeh2006@gmail.com>
2021-07-06 01:26:49 +0300

    Add Android Controller

--- main.cpp
+++ main.cpp
@@ -18,7 +18,17 @@
 #include "lib/Scene.hpp"
 #include "lib/Resources.hpp"
 
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#define PRESSED 1
+#define NOT_PRESSED 0
+
 #include <unistd.h>
+#include <string>
+#include <sstream>
+#include <thread>
+#include <chrono>
 
 Display display;
 
@@ -39,6 +49,9 @@ std::string LOAD_MODELS_ERROR  = "No Errors",
             MAKE_DISPLAY_ERROR = "No Errors",
             LOAD_SHADER_ERROR  = "No Errors";
 
+FT_Library ft_lib;
+FT_Face face;
+FT_GlyphSlot slot;
 
 //std::string model_path = "/home/mdhazim/Blender/untitled.obj";
 std::string model_path = "res/dragon.obj";
@@ -86,8 +99,8 @@ bool MakeDisplay() {
     return true;
 }
 
-
 bool Setup() {
+    // load all obj models before making display to avoid displat overtime
     Debugger::Log("Game::Startup", "Loading OBJ Models before Initializing Display ...");
     if (! loadModels()) {
         Debugger::Error("Game::Startup", "Failed to load Models, models Loader error:\n\t" +
@@ -96,9 +109,11 @@ bool Setup() {
         return false;
     }
 
+    // Log 
     Debugger::Log("Game::Setup", "All OBJ Models Loaded");
     Debugger::Log("Game::Setup", "Making 512x512 Display ...");
 
+    // make display from local function
     if (! MakeDisplay()) {
         Debugger::Error("Game::Setup", "Failed to Make Display, display manager error:\n\t"+
             MAKE_DISPLAY_ERROR);
@@ -106,13 +121,18 @@ bool Setup() {
         return false;
     }
 
+
+    // add display pointer for keyboard and mouse
     Keyboard::setDisplay(&display);
     Mouse::setDisplay(&display);
 
     Debugger::Log("Game::Setup", "Making Stall Render Object ...");
 
+
+    // make static unsigned int for static draw model
     unsigned int usage = GL_STATIC_DRAW;
 
+    // init model va and add va layouts and buffers
     stall.va.Init();
     stall.va.AddLayout(3, stall_loader.vertices_pointer(), stall_loader.vertices_size * FLOAT_SIZE, usage);
     stall.va.AddLayout(2, stall_loader.uvs_pointer(), stall_loader.uvs_size * FLOAT_SIZE, usage);
@@ -120,26 +140,32 @@ bool Setup() {
 
     Debugger::Log("Game::Setup", "Making Static Shader ...");
 
+
+    // load shader from resources
     if (! shader.loadFromFile(Res::get("static_shader.hlsl"))) {
         Debugger::Error("Game::Setup", "Failed to load Shader");
         return false;
     }
 
+    // compile shader
     if (! shader.Compile()) {
         Debugger::Error("Game::Setup", "Failed to Compile Shader Program, more info:\n"
             + shader.error);
         return false;
     }
 
-    stall.model.Translate(0, 0, 0);    
+
+    // translate stall model and view
+    //stall.model.Translate(0, 0, -10);    
     view.Translate(0, -1, -10);
 
-    projection.setProjection_Perspictive(60, 1, 0.1, 100);
-    light.lightColor = Colors::WHITE;
-    //light.lightColor = Colors::White(150);
-    light.brightness = 0.8;
-    stall.texture = CreateTexture(Res::get("texs/stallTexture.png"));
 
+    // set projection to "perspictive"
+    projection.setProjection_Perspictive(60, 1, 0.1, 100);
+    stall.texture = TextureBuffers::Make(45, 189, 2, 255, 512, 512);
+
+
+    // init sphere va and add va layouts and buffers
     sphere.va.Init();
     sphere.va.AddLayout(3, sphere_loader.vertices_pointer(), sphere_loader.vertices_size * FLOAT_SIZE, usage);
     sphere.va.AddLayout(2, sphere_loader.uvs_pointer(), sphere_loader.uvs_size * FLOAT_SIZE, usage);
@@ -147,11 +173,29 @@ bool Setup() {
 
     glfwSetInputMode(display.glfwWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
+    
+    // make gameScene object and name it
     gameScene.setSceneName("main_scene");
 
+    // add model and sphere to game scene
     gameScene.addRenderObject(makeSceneRenderObject("obj_model", stall.Pointer()));
     gameScene.addRenderObject(makeSceneRenderObject("sphere", sphere.Pointer()));
 
+
+    // init Freetype library
+    if (FT_Init_FreeType(&ft_lib)) {
+        std::cout << "Failed to init FT Lib\n";
+        exit(-1);
+    }
+
+
+    // init minecraft font face
+    if(FT_New_Face(ft_lib, "res/fonts/Minecraft.ttf", 0, &face)) {
+        std::cout << "Failed to load font\n";
+        
+    }
+
+    slot = face->glyph;
 
     return true;
 }
@@ -169,103 +213,204 @@ void RenderFrame() {
     display.SwapBuffers();
 }
 
+bool ANDROID_CONTROLLER_LOOP = true;
+
+namespace AndroidController {
+    int horizontal = 0, vertical = 0, up = 0, down = 0, left = 0, right = 0, a = 0, b = 0, c = 0, d = 0;
+}
+
+void AndroidController_THREAD() {
+	std::string from = "SensorController Thread";
+	Debugger::Log(from, "Sensor Controller Thread");
+
+	std::ifstream ifs;
+	std::string dev_path =  "/home/mdhazim/FlaskProjects/AndroidController/dev";
+	std::string line;
+
+    float last_time = 0;
+
+	while(ANDROID_CONTROLLER_LOOP) {
+		// open file
+		ifs.open(dev_path);
+
+        std::getline(ifs, line);
+        AndroidController::horizontal = std::atoi(line.c_str());
+
+        std::getline(ifs, line);
+        AndroidController::vertical = std::atoi(line.c_str());
+
+        std::getline(ifs, line);
+        AndroidController::up = std::atoi(line.c_str());
+
+        std::getline(ifs, line);
+        AndroidController::down = std::atoi(line.c_str());
+
+        std::getline(ifs, line);
+        AndroidController::right = std::atoi(line.c_str());
+
+        std::getline(ifs, line);
+        AndroidController::left = std::atoi(line.c_str());
+
+        std::getline(ifs, line);
+        AndroidController::a = std::atoi(line.c_str());
+
+        std::getline(ifs, line);
+        AndroidController::b = std::atoi(line.c_str());
+
+        std::getline(ifs, line);
+        AndroidController::c = std::atoi(line.c_str());
+
+        std::getline(ifs, line);
+        AndroidController::d = std::atoi(line.c_str());
+
+        ifs.close();
+
+        //printf("Hello\n");
+	}
+}
+
 
 ActionManager EnableTextureAction, ChangeTranslateObject;
-bool textureEnabled = true;
+bool textureEnabled = false;
 MatrixObject* translate_object;
 
+struct Vec3 {
+    float x, y, z;
+
+    Vec3(float _x, float _y, float _z) {
+        x = _x;
+        y = _y;
+        z = _z;
+    }  
+};
+
+GLenum gl_error;
+
+bool checkGLError() {
+    if ((gl_error = glGetError()) != GL_NO_ERROR){
+        Debugger::Error("Check_GL_Error", "OpenGL Error : \n\tCode : " + std::to_string((int) gl_error));
+        return false;
+    }
+
+    return true;
+}
 
 void Render_01() {
-    VertexArray quad;
-
-    std::vector<glm::vec3> vertices {
-        glm::vec3(-0.5, -0.5, 0),
-        glm::vec3(-0.5, 0.5, 0),
-        glm::vec3(-0.5, 0.5, 0),
-    };
-
-    quad.Init();
-    //quad.AddLayout(3, &vertices[0].x, vertices.size() * VEC_SIZE, GL_STATIC_DRAW);
-    glGenBuffers(1, &quad.VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, quad.VBO);
-
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 12, nullptr);
-    
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * VEC_SIZE, &vertices[0].x, GL_STATIC_DRAW);
-
-    //quad.LoadElementArrayBuffer(&indices[0], 6, GL_STATIC_DRAW);
-
-    while(! display.shouldClose() && !Keyboard::isPressed(GLFW_KEY_Q)) {
-        Clear();
-
-        quad.Bind();
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-      
-
-        RenderFrame();
-    }
+   	
 }
 
 void Render() {
+    // Print Graphics Driver Information
     Debugger::Log("Driver Information", "OpenGL Version : " + std::string(display.opengl_Version()));
     Debugger::Log("Driver Information", "Graphics Renderer : " + std::string(display.graphicsRenderer()));
     Debugger::Log("Driver Information", "Graphics Vendor : " + std::string(display.graphicsVendor()));
     
-    //stall.model.Rotate(45, 0, 1, 0);
-    //stall.model.Scale(1.5, 1.5, 1.5);
 
+    // Change light
     light.lightColor = Colors::GOLD;
-	light.position.Translate(0, 0, 25);
-
+	light.position.Translate(0, 0, 10);
     translate_object = &view;
 
+
+    ActionManager changeCursorMode;
+
+    // set names for actions
     EnableTextureAction.setName("enable_texture");
     ChangeTranslateObject.setName("change_translate_obj");
+    changeCursorMode.setName("ccm");
 
+    view.Translate(0, 0, 0);
+
+
+    // make actions
     Actions actions;
 
+
+    // add all actions to actions vector
     actions.addAction(EnableTextureAction.Pointer());
     actions.addAction(ChangeTranslateObject.Pointer());
+    actions.addAction(changeCursorMode.Pointer());
+
+
+    // make texture surface
+
+    TextureSurface tex_surf;
+    //tex_surf.setTexture(TextureBuffers::Make(255, 0, 0, 255, 64, 64));
+    tex_surf.setTexture(Res::get("texs/opengl.jpg"));
+    tex_surf.Make();
+
+    //stall.model.Translate(0, 0, -20);
+
+	std::thread AndroidControllerThread(AndroidController_THREAD);
+
+    view.Translate(0, 0, -10);
+
+    int cursorMode = GLFW_CURSOR_DISABLED;
 
     while(! display.shouldClose() && ! Keyboard::isPressed(GLFW_KEY_Q)) {
         Clear();
 
+
+        // Increase all actions
         actions.IncreaseAll(display.delta_time);
 
-        if (actions["change_translate_obj"]->isAvailable(0.5) && Keyboard::isPressed(GLFW_KEY_C)) {
-            actions["change_translate_obj"]->Zero();
-            translate_object = (translate_object == &light.position ? &view : &light.position);
+        if (changeCursorMode.isAvailable(0.3) && Keyboard::isPressed(GLFW_KEY_TAB)) {
+            cursorMode = (cursorMode == GLFW_CURSOR_DISABLED ? GLFW_CURSOR_NORMAL : GLFW_CURSOR_DISABLED);
+            glfwSetInputMode(display.glfwWindow, GLFW_CURSOR, cursorMode);
+
+            changeCursorMode.Zero();
         }
 
-        translate_object->Translate(
-            -Keyboard::horizontal() * player_speed * display.delta_time,
-            -(Keyboard::isPressed(GLFW_KEY_PAGE_UP) ? 1 : ( (Keyboard::isPressed(GLFW_KEY_PAGE_DOWN) ? -1 : 0) )) * player_speed * display.delta_time,
-            Keyboard::vertical() * player_speed * display.delta_time
+
+        // Move Translate Object By Keyboard Data
+
+        //std::cout << AndroidController::horizontal << ", " << AndroidController::vertical << "\n";
+
+        view.Translate(
+            //-Keyboard::horizontal() * player_speed * display.delta_time,
+			player_speed * display.delta_time * -AndroidController::horizontal,
+            -(AndroidController::a?1:((AndroidController::b ? -1 : 0)))*player_speed*display.delta_time,
+            player_speed * display.delta_time * AndroidController::vertical
         );
 
-		stall.model.Rotate(display.delta_time * rotation_speed, 0, 1, 0);
 
+        float rotation_value = 0;
+
+        if (AndroidController::c == PRESSED) rotation_value --;
+        if (AndroidController::d == PRESSED) rotation_value ++;
+
+        std::cout << "Rotation value : " << rotation_value << "\n";
+
+        if (rotation_value != 0) stall.model.Rotate(
+            rotation_speed * display.delta_time * rotation_value,
+            0, 1, 0
+        );
+
+
+        // Rotate Model by mouse delta_x if user pressed mouse right button
         if (glfwGetMouseButton(display.glfwWindow, GLFW_MOUSE_BUTTON_MIDDLE)) stall.model.Rotate(
             Mouse::delta_x * (rotation_speed / 2) * display.delta_time,
             0, 1, 0
         );
 
+
+        // Reset model and light matrix
         if (glfwGetMouseButton(display.glfwWindow, GLFW_MOUSE_BUTTON_RIGHT)) {
             stall.model.Reset();
             light.position.Reset();
-            //view.Reset();
         } 
 
+
+        // Disable or Enable Texture if user clicked T Button
         if (Keyboard::isPressed(GLFW_KEY_T) && EnableTextureAction.isAvailable(0.3)) {
             EnableTextureAction.Zero();
             textureEnabled = !textureEnabled;
         }
 
+        // Disable Blend before drawing 
         glDisable(GL_BLEND);
 
-        /* Render all */
-        //shader.UniformVec4f("light_pos", 0, 0, 1, 1);
+        // Uniform Static Uniforms for shader
         shader.Use();
         shader.UniformMatrix("view",  view.Pointer());
         shader.UniformMatrix("projection", projection.Pointer());
@@ -275,28 +420,59 @@ void Render() {
         shader.UniformVec4f("light_color", light.lightColor);
         shader.Disable("enable_blend");
         shader.UniformInteger("enable_texture", (textureEnabled ? 1 : 0));
-        stall.texture.Bind();
+
+        // Bind Model texture and vertex array
+        stall.texture.Bind(); 
         stall.va.Bind();
         
+        // uniform model Matrix
         shader.UniformMatrix("model", stall.model.Pointer());
 
+        // draw triangles arrays if model is drawable
         if (stall.Drawable) glDrawArrays(GL_TRIANGLES, 0, stall_loader.faces);
-        //glDrawElements(GL_TRIANGLES, 0, GL_UNSIGNED_INT, nullptr);
 
+        // uniform sphere data
         shader.UniformMatrix("model", light.position.Pointer());
         shader.UniformVec4f("light_color", 1, 1, 1, 1);
         shader.UniformInteger("brightness", 100);
         shader.Disable("enable_texture");
 
+        // bind shere vertex array
         sphere.va.Bind();
 
+        // draw sphere if drawable
         if (sphere.Drawable) glDrawArrays(GL_TRIANGLES, 0, sphere_loader.faces);
 
-        glEnable(GL_BLEND);
+        // Draw texture surface
+
+        glDisable(GL_DEPTH_TEST);
+
+        tex_surf.shader.Use();
+
+        tex_surf.va.Bind();
+        tex_surf.texture.Bind();
+
+        tex_surf.model.Translate(display.delta_time * 10, 0, 0);
+
+        tex_surf.shader.UniformMatrix("projection", glm::ortho(-5, 5, -5, 5));
+        tex_surf.shader.UniformMatrix("position", tex_surf.model.Pointer());
+
+        glDrawArrays(GL_QUADS, 0, 4);
+
+        glEnable(GL_DEPTH_TEST);
 
         /* render frame buffer */
         RenderFrame();
     }
+
+	ANDROID_CONTROLLER_LOOP = false;
+
+	// Join Thread to avoid (abort)
+	Debugger::Log("Render", "Waiting for Android controller Thread to close ...");
+	AndroidControllerThread.join();
+	Debugger::Log("Render", "Android Controller Thread closed");
+
+	return;
 }
 
 
@@ -313,3 +489,13 @@ int main(int c, char ** args) {
 
     exit(1);
 }
+
+
+
+
+
+
+
+
+
+
